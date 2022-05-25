36eb3f858509891a7481f9253048133f57189234
sazzad <sazzadkarim01@gmail.com>
2022-05-25 20:29:32 +0600

    initial setup commited

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// X8t0XuAXrduuKvvC
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = "mongodb+srv://laptop_admin:X8t0XuAXrduuKvvC@cluster0.zixex.mongodb.net/?retryWrites=true&w=majority";
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db("laptop").collection("products")
+
+      
+        
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("LAPTOP SERVER SIDE!");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Laptop listening on port ${port}`);
+  });
