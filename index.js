2e4e727e01ee71aa08db690dcc9632598d19d6fd
sazzad <sazzadkarim01@gmail.com>
2022-05-25 20:39:41 +0600

    commit code for deletee products

--- index.js
+++ index.js
@@ -72,6 +72,15 @@ async function run() {
         })
 
 
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result)
+        
+        })
+
+
 
     }
     finally {
